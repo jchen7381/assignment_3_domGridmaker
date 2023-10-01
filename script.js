c46e633b8cb2f1a1ef483714415c4d8f7dc3fea1
ceonz <ceon91@gmail.com>
2023-10-01 11:45:22 -0400

    color selection, toggle, extracrdit: color-wheel

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let selectedColor = "red"; // Default color
+document.addEventListener("DOMContentLoaded", function () {
+  let selectedColor = "black"; // Default color
   const grid = document.getElementById("grid");
+  const cssRoot = document.querySelector(":root")
+  cssRoot.style.setProperty("--selected-color", selectedColor)
 
-  document.getElementById("addRow").addEventListener("click", function() {
+  document.getElementById("addRow").addEventListener("click", function () {
     const newRow = document.createElement("tr");
     const numCols = grid.querySelector("tr")
       ? grid.querySelector("tr").children.length
@@ -14,26 +16,26 @@ document.addEventListener("DOMContentLoaded", function() {
     grid.appendChild(newRow);
   });
 
-  document.getElementById("addColumn").addEventListener("click", function() {
+  document.getElementById("addColumn").addEventListener("click", function () {
     grid.querySelectorAll("tr").forEach((row) => {
       const newCell = document.createElement("td");
       row.appendChild(newCell);
     });
   });
 
-  document.getElementById("removeRow").addEventListener("click", function() {
+  document.getElementById("removeRow").addEventListener("click", function () {
     const numRows = grid.querySelectorAll("tr").length;
     if (numRows > 0) {
       grid.lastChild.remove();
     }
   });
 
-  document.getElementById("removeColumn").addEventListener("click", function() {
+  document.getElementById("removeColumn").addEventListener("click", function () {
     const rows = grid.querySelectorAll("tr");
-    if(rows.length > 0) {
+    if (rows.length > 0) {
       rows.forEach(row => {
         const cells = row.querySelectorAll("td");
-        if(cells.length > 0) {
+        if (cells.length > 0) {
           const lastCell = cells[cells.length - 1];
           row.removeChild(lastCell);
         }
@@ -41,17 +43,29 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  document.getElementById("removeColumn").addEventListener("click", function() {
+  document.getElementById("removeColumn").addEventListener("click", function () {
     const rows = grid.querySelectorAll("tr");
-    if(rows.length > 0) {
+    if (rows.length > 0) {
       rows.forEach(row => {
         const cells = row.querySelectorAll("td");
-        if(cells.length > 0) {
+        if (cells.length > 0) {
           const lastCell = cells[cells.length - 1];
           row.removeChild(lastCell);
         }
       });
     }
   });
+
+  document.getElementById("colorPicker").addEventListener("input", function (event) {
+    selectedColor = event.target.value
+    cssRoot.style.setProperty("--selected-color", selectedColor)
+  })
+
+  document.getElementById("grid").addEventListener("click", function (event) {
+    if (event.target.tagName === "TD") {
+      event.target.classList.toggle("selected")
+    }
+
+  })
 
 });
